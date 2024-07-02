d89a32bf234c8b2cc3029a193fcf7ae8e3f493dd
Fisayo <aikomofisayo@gmail.com>
2024-07-02 11:43:06 +0100

    chore: Update Jest configuration and add testing dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
-  testMatch: ['**/tests/**/*.test.ts'],
+  testMatch: ['**/tests/**/*.test.ts?(x)'],
   transform: {
     '^.+\\.ts$': [
       'ts-jest',
