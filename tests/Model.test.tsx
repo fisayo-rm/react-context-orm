a7c54b9dfaa8938ba954cb6d772d8d032f779aa6
Fisayo <aikomofisayo@gmail.com>
2024-07-02 17:59:53 +0100

    feat: Add test for insert method

--- tests/Model.test.tsx
+++ tests/Model.test.tsx
@@ -24,7 +24,7 @@ const TestComponent = ({ payload }: { payload: any }) => {
     TestModel.init(state, dispatch);
     const createModel = async () => {
       // await TestModel.init(state, dispatch);
-      await TestModel.create({ data: payload });
+      await TestModel.create(payload);
     };
     createModel();
   }, []);
@@ -32,6 +32,54 @@ const TestComponent = ({ payload }: { payload: any }) => {
   // useEffect(() => {
   //   TestModel.store = state;
   // }, [state]);
+
+  return (
+    <div>
+      <div data-testid="state">{JSON.stringify(state)}</div>
+    </div>
+  );
+};
+
+const InsertComponent = ({ payload }: { payload: any }) => {
+  const state = useStoreState();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    TestModel.init(state, dispatch);
+
+    const insertModel = async () => {
+      await TestModel.insert(payload);
+    };
+    insertModel();
+  }, []);
+
+  return (
+    <div>
+      <div data-testid="state">{JSON.stringify(state)}</div>
+    </div>
+  );
+};
+
+const CreateAndInsertComponent = ({
+  createPayload,
+  insertPayload,
+}: {
+  createPayload: any;
+  insertPayload: any;
+}) => {
+  const state = useStoreState();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    TestModel.init(state, dispatch);
+
+    const createAndInsertModel = async () => {
+      await TestModel.create(createPayload);
+      await TestModel.insert(insertPayload);
+      // await TestModel.create(createPayload);
+    };
+    createAndInsertModel();
+  }, []);
 
   return (
     <div>
@@ -57,7 +105,7 @@ test('Model should create an instance with given values', () => {
 });
 
 test('Model should dispatch create action with payload and update state', async () => {
-  const payload = { id: 1, name: 'test' };
+  const payload = { data: { id: 1, name: 'test' } };
 
   const { getByTestId } = render(
     <StoreProvider>
@@ -75,9 +123,9 @@ test('Model should dispatch create action with payload and update state', async 
 });
 
 test('Model static store property should be updated', async () => {
-  const payload = { id: 2, name: 'test2' };
+  const payload = { data: { id: 2, name: 'test2' } };
 
-  const { getByTestId } = render(
+  render(
     <StoreProvider>
       <TestComponent payload={payload} />
     </StoreProvider>,
@@ -85,13 +133,68 @@ test('Model static store property should be updated', async () => {
 
   await waitFor(() => {
     // Log the store property for debugging
-    console.log('TestModel Store:', TestModel.store);
-    console.log('Model Store:', Model.store);
+    // console.log('TestModel Store:', TestModel.store);
+    // console.log('Model Store:', Model.store);
 
     // Verify the static store property is updated
     const state = Model.store;
     const instance = state.test.find((item: TestModel) => item.id === 2);
     expect(instance).toBeDefined();
     expect(instance.name).toBe('test2');
+  });
+});
+
+test('Model should insert records and update state', async () => {
+  const payload = {
+    data: [
+      { id: 3, name: 'test3' },
+      { id: 4, name: 'test4' },
+    ],
+  };
+
+  render(
+    <StoreProvider>
+      <InsertComponent payload={payload} />
+    </StoreProvider>,
+  );
+
+  await waitFor(() => {
+    const state = Model.store;
+    expect(state.test.length).toBe(2);
+    expect(state.test.find((item: Model) => item.id === 3)).toBeDefined();
+    expect(state.test.find((item: Model) => item.id === 4)).toBeDefined();
+  });
+});
+
+test('Model should create and insert records and update state', async () => {
+  const createPayload = { data: { id: 1, name: 'test1' } };
+  const insertPayload = {
+    data: [
+      { id: 2, name: 'test2' },
+      { id: 3, name: 'test3' },
+    ],
+  };
+
+  render(
+    <StoreProvider>
+      <CreateAndInsertComponent
+        createPayload={createPayload}
+        insertPayload={insertPayload}
+      />
+    </StoreProvider>,
+  );
+
+  await waitFor(() => {
+    const state = Model.store;
+
+    expect(state.test.length).toBe(3);
+    expect(state.test.find((item: Model) => item.id === 1)).toBeDefined();
+    expect(state.test.find((item: Model) => item.id === 2)).toBeDefined();
+    expect(state.test.find((item: Model) => item.id === 3)).toBeDefined();
+
+    // expect(state.test.length).toBe(1);
+    // expect(state.test.find((item: Model) => item.id === 1)).toBeDefined();
+    // expect(state.test.find((item: Model) => item.id === 2)).toBeUndefined();
+    // expect(state.test.find((item: Model) => item.id === 3)).toBeUndefined();
   });
 });
