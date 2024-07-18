e995271440cc0c2c9dd4bd91f8850dee72dc985d
Fisayo <aikomofisayo@gmail.com>
2024-07-18 16:51:36 +0100

    Refactor QueryBuilder.ts to use a modular approach and add JSDoc comments

--- src/QueryBuilder.ts
+++ src/QueryBuilder.ts
@@ -1,18 +1,27 @@
 import { Model } from './Model';
+import { QueryBuilderOptions } from './queryBuilderInterfaces';
 
-interface QueryBuilderOptions {
-  relations: { [key: string]: (qb: QueryBuilder<any>) => void };
-  order: { field: string; direction: 'asc' | 'desc' }[];
-}
-
+/**
+ * Class representing a query builder for models.
+ */
 export class QueryBuilder<T extends typeof Model> {
   private modelClass: T;
   private options: QueryBuilderOptions = { relations: {}, order: [] };
 
+  /**
+   * Creates an instance of QueryBuilder.
+   * @param modelClass The model class to build queries for.
+   */
   constructor(modelClass: T) {
     this.modelClass = modelClass;
   }
 
+  /**
+   * Specifies relationships to be loaded with the model.
+   * @param relations The relationships to load.
+   * @param query Optional query function to apply to the relationship.
+   * @returns The query builder instance.
+   */
   with(
     relations: string | string[],
     query?: (qb: QueryBuilder<any>) => void,
@@ -27,11 +36,22 @@ export class QueryBuilder<T extends typeof Model> {
     return this;
   }
 
+  /**
+   * Specifies the order for the query results.
+   * @param field The field to order by.
+   * @param direction The direction to order by (asc or desc).
+   * @returns The query builder instance.
+   */
   orderBy(field: string, direction: 'asc' | 'desc' = 'asc'): this {
     this.options.order.push({ field, direction });
     return this;
   }
 
+  /**
+   * Finds a model instance by its ID.
+   * @param id The ID of the model instance.
+   * @returns The found model instance or undefined.
+   */
   find(id: any): InstanceType<T> | undefined {
     const instance = this.modelClass.find(id);
 
@@ -43,6 +63,11 @@ export class QueryBuilder<T extends typeof Model> {
     return instanceCopy;
   }
 
+  /**
+   * Creates a copy of the model instance.
+   * @param instance The model instance to copy.
+   * @returns The copied model instance.
+   */
   private copyInstance(instance: InstanceType<T>): InstanceType<T> {
     const copy = Object.assign(
       Object.create(Object.getPrototypeOf(instance)),
@@ -51,6 +76,10 @@ export class QueryBuilder<T extends typeof Model> {
     return copy;
   }
 
+  /**
+   * Loads the specified relationships for the model instance.
+   * @param instance The model instance to load relationships for.
+   */
   private loadRelations(instance: InstanceType<T>) {
     for (const relation in this.options.relations) {
       const parts = relation.split('.');
@@ -79,6 +108,10 @@ export class QueryBuilder<T extends typeof Model> {
     }
   }
 
+  /**
+   * Applies the specified order to the query results.
+   * @param items The items to order.
+   */
   private applyOrder(items: any[]) {
     this.options.order.forEach(({ field, direction }) => {
       items.sort((a: any, b: any) => {
---
+++ src/queryBuilderInterfaces.ts
@@ -0,0 +1,9 @@
+import { QueryBuilder } from './QueryBuilder';
+
+/**
+ * Interface representing the options for a query builder.
+ */
+export interface QueryBuilderOptions {
+  relations: { [key: string]: (qb: QueryBuilder<any>) => void };
+  order: { field: string; direction: 'asc' | 'desc' }[];
+}
